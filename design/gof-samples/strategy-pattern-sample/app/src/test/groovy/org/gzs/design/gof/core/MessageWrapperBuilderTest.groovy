0d45ab7e1af4a91305792e211223fd133c5cb24d
Gerardo Zúñiga Sánchez <gerardo.blupub@gmail.com>
2025-02-16 21:36:53 -0600

    Red: Defined the unit test for getting a MessageWrapperBuilder

---
+++ design/gof-samples/strategy-pattern-sample/app/src/test/groovy/org/gzs/design/gof/core/MessageWrapperBuilderTest.groovy
@@ -0,0 +1,10 @@
+package org.gzs.design.gof.core
+
+import spock.lang.Specification
+
+class MessageWrapperBuilderTest extends Specification {
+    def "Gets an instance of MessageWrapperBuilder"() {
+        expect:
+        MessageWrapper.builder() intanceof MessageWrapperBuilder
+    }
+}
